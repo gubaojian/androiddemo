a2b4c196a7966e46bd82ca306a8890740a3877d4
baojian <baojian.gu@longbridge-inc.com>
2026-04-03 16:57:03 +0800

    no message

---
+++ ComposeLearn2/app/src/main/java/com/example/composelearn/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.example.composelearn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composelearn.ui.theme.ComposeLearnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeLearnTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun GreetingCard(name: String, desc: String) {
+    Row(
+        modifier = Modifier.padding(20.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier.clip(CircleShape)
+        )
+        Column(
+            modifier = Modifier.padding(start = 20.dp)
+        ) {
+            Text(
+                text = "Hello $name!"
+            )
+            Text(
+                text = "Hello $desc!"
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val messages = listOf(
+        "hello",
+        "2",
+        "3",
+        "3",
+        "hello",
+        "2",
+        "3",
+        "3",
+        "hello",
+        "2",
+        "3",
+        "3",
+        "hello",
+        "2",
+        "3",
+        "3",
+        "hello",
+        "2",
+        "3",
+        "3",
+        "hello",
+        "2",
+        "3",
+        "3",
+        "hello",
+        "2",
+        "3",
+        "3"
+    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        GreetingCard(
+            name = "gubaojian",
+            desc = "desc"
+        )
+        Box(
+            modifier = Modifier
+                .weight(1.0f)
+                .fillMaxWidth()
+        ) {
+            LazyColumn() {
+                items(messages) { message ->
+                    GreetingCard(name = message, desc = "desc ${message}")
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .weight(1.0f)
+                .fillMaxWidth()
+        ) {
+            LazyColumn() {
+                items(messages) { message ->
+                    GreetingCard(name = message, desc = "desc ${message}")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeLearnTheme {
+        Greeting("Android")
+    }
+}
