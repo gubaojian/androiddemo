a0a8f184b1b8720d781071a3bc937b14c1dd9909
baojian <baojian.gu@longbridge-inc.com>
2026-04-21 15:44:45 +0800

    [feat] transition动画

--- ComposeLearn2/app/src/main/java/com/example/composelearn/ComposeLearn2Main.kt
+++ ComposeLearn2/app/src/main/java/com/example/composelearn/ComposeLearn2Main.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Transition
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateRect
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,8 +28,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import com.example.composelearn.ui.theme.ComposeLearnTheme
+import com.king.ultraswiperefresh.indicator.animationSpec
+import kotlin.properties.ReadOnlyProperty
 
 class ComposeLearn2Main : AppCompatActivity() {
 
@@ -33,6 +49,8 @@ class ComposeLearn2Main : AppCompatActivity() {
                         Text("hello world")
                         HelloAnimation()
                         HelloAnimation2()
+                        HelloWorld()
+                        HelloWorld2()
                     }
                 }
             }
@@ -70,6 +88,70 @@ fun HelloAnimation2() {
             Box(modifier = Modifier.background(Color.Red)) {
                 Text("hello world animatedvisibility ${animationState}")
             }
+        }
+    }
+}
+
+@Composable
+fun HelloWorld() {
+    var startAnimation by remember { mutableStateOf(false) }
+    val animationAlpha by animateFloatAsState(
+        targetValue = if (startAnimation) 1.0f else 0.1f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+        .alpha(animationAlpha)
+    ) {
+        Box(modifier = Modifier.fillMaxSize()
+        .background(Color.Cyan)) {
+            Button(onClick = {
+                startAnimation = !startAnimation
+            }) {
+                Text("start animation")
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloWorld2() {
+    var stateAnimation by remember { mutableStateOf(false) }
+    val transition = updateTransition(stateAnimation)
+    val rect by transition.animateRect(
+        transitionSpec =  {
+            tween(durationMillis = 3000)
+        }
+    ) { state ->
+        if (state) {
+            Rect(0f,0f, 400f, 100f)
+        } else {
+            Rect(0f,0f, 200f, 50f)
+        }
+    }
+    val color by transition.animateColor(
+        transitionSpec =  {
+            tween(durationMillis = 3000)
+        }
+    ) { state ->
+         if (state) {
+             Color.Yellow
+         } else {
+             Color.Blue
+         }
+    }
+    Box(modifier = Modifier
+        .height(rect.height.dp)
+        .width(rect.width.dp)
+        .background(color)
+    ) {
+        Button(onClick = {
+            stateAnimation = !stateAnimation
+        }) {
+            Text("start animation")
         }
     }
 }
