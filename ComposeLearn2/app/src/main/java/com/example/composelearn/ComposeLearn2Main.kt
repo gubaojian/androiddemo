5ee5a1fee7b5aa0932302f6d6ef502469187cd86
baojian <baojian.gu@longbridge-inc.com>
2026-04-21 15:13:15 +0800

    [feat] animation state

--- ComposeLearn2/app/src/main/java/com/example/composelearn/ComposeLearn2Main.kt
+++ ComposeLearn2/app/src/main/java/com/example/composelearn/ComposeLearn2Main.kt
@@ -3,11 +3,21 @@ package com.example.composelearn
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.composelearn.ui.theme.ComposeLearnTheme
 
 class ComposeLearn2Main : AppCompatActivity() {
@@ -20,10 +30,28 @@ class ComposeLearn2Main : AppCompatActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column() {
                         Text("hello world")
+                        HelloAnimation()
                     }
                 }
             }
         }
     }
 
+}
+
+@Composable
+fun HelloAnimation() {
+    var expandState by remember { mutableStateOf(false) }
+    Box(modifier =
+        Modifier.background(Color.Blue).animateContentSize().clickable() {
+            expandState = !expandState
+        }
+    ) {
+        Column() {
+            Text("hello world")
+            if (expandState) {
+                Text("hello world expand animation")
+            }
+        }
+    }
 }
