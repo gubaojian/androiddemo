73cb3f413221c9e371811f7330d76a5c42233f36
baojian <baojian.gu@longbridge-inc.com>
2026-05-07 14:40:50 +0800

    produceState用法

--- MVI/app/src/main/java/com/example/mvi/ComposeActivity.kt
+++ MVI/app/src/main/java/com/example/mvi/ComposeActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mvi
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,16 +21,22 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mvi.ui.theme.MVITheme
+import kotlinx.coroutines.delay
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +44,10 @@ class ComposeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MVITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.Red
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -45,21 +58,66 @@ class ComposeActivity : ComponentActivity() {
     }
 }
 
+sealed class DetailUiState {
+    object Loading: DetailUiState()
+    data class Success(val message: String): DetailUiState()
+    data class Error(val msg: String) : DetailUiState()
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var openDialogWithImage by remember { mutableStateOf(false) }
     var expanded by remember { mutableStateOf(false) }
 
+    val uiState = produceState<DetailUiState>(initialValue = DetailUiState.Loading) {
+        try {
+            delay(1000)
+            value = DetailUiState.Success("加载成功")
+        }catch (e: Exception) {
+            value = DetailUiState.Error("加载失败${e}")
+        }
+    }
+
+    val stateValue = uiState.value
+
     Column() {
         Text(
             text = "Hello $name!",
             modifier = modifier
         )
+        when (stateValue) {
+            is DetailUiState.Loading -> {
+                Text("加载中")
+            }
+            is DetailUiState.Success -> {
+                Text("加载成功 ${stateValue.message}")
+            }
+            is DetailUiState.Error -> {
+                Text("加载失败 ${stateValue.msg}")
+            }
+        }
         Button(onClick = {
             openDialogWithImage = true
         }) {
             Text("click me, open dialog")
+        }
+        AnimatedVisibility(
+            expanded
+            ,
+            enter = slideInVertically(
+                initialOffsetY = {fullHeight -> -fullHeight}
+            ),
+            exit = slideOutVertically(
+                targetOffsetY = { fullHeight -> -fullHeight}
+            )
+        ) {
+            Surface(
+                color = MaterialTheme.colorScheme.secondary
+            ) {
+                Text("hello world",
+                    modifier = Modifier.padding(16.dp))
+            }
         }
     }
     if (openDialogWithImage) {
