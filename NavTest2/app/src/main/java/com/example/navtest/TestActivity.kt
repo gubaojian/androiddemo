5a4483f2339afac303473208d6d6a5254c65ec0a
gubaojian <gubaojian@tngdigital.com.my>
2026-08-03 17:30:43 +0800

    feat(EW-352184): nav test case

--- NavTest2/app/src/main/java/com/example/navtest/TestActivity.kt
+++ NavTest2/app/src/main/java/com/example/navtest/TestActivity.kt
@@ -1,12 +1,21 @@
 package com.example.navtest
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.tooling.data.Group
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.badge.BadgeUtils
+import com.google.android.material.badge.ExperimentalBadgeUtils
 
 class TestActivity : AppCompatActivity() {
+    @OptIn(ExperimentalBadgeUtils::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,18 @@ class TestActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val badgeDrawable = BadgeDrawable.create(this)
+        badgeDrawable.text  = "100"
+        badgeDrawable.isVisible = true
+        badgeDrawable.badgeGravity = BadgeDrawable.TOP_END
+        val badgeContainer = findViewById<FrameLayout>(R.id.badgeContainer)
+        badgeContainer.post {
+            BadgeUtils.attachBadgeDrawable(badgeDrawable,
+                findViewById<View>(R.id.badge),
+                null
+            )
         }
     }
 }
