de3fbaf9b938c3b31c82ac718bcd2d30002c4103
gubaojian <gubaojian@tngdigital.com.my>
2026-08-13 18:23:48 +0800

    make to standard 56dp

--- zhongpin_android/app/src/main/java/com/zhongpin/mvvm_android/ui/main/MainActivity.kt
+++ zhongpin_android/app/src/main/java/com/zhongpin/mvvm_android/ui/main/MainActivity.kt
@@ -206,7 +206,6 @@ class MainActivity  : BaseVMActivity<MainViewModel>() {
         tabs.add(Tab(this@MainActivity, "我的", R.mipmap.tab_my).setFocusIcon(this, R.mipmap.tab_my_current));            //使用 setFocusIcon(bitmap/drawable/resId) 来添加选中时的第二套图标
         binding.tabs.setTab(tabs)
 
-
         binding.tabs.setOnTabChangeListener(
             object: OnTabChangeListener {
                 override fun onTabChanged(v: View?, index: Int): Boolean {
